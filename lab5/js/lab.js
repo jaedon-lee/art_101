493ffc36e178d95f48d4d46f38cb5d14c28013de
jaedon-lee <67574588+jaedon-lee@users.noreply.github.com>
2021-01-27 13:21:03 -0800

    commit

---
+++ lab5/js/lab.js
@@ -0,0 +1,18 @@
+/**
+ * Author:    Jaedon Lee
+ * Created:   1/27/21
+ * License: Public Domain
+ *
+ **/
+
+ make = "Toyota";
+ model = "Prius";
+ color = "Black";
+ year = "2005";
+ age = 2021 - year;
+
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
