17db3a0927eb4365226382c7bcd29ea04c9d0119
jaedon-lee <67574588+jaedon-lee@users.noreply.github.com>
2021-02-01 22:05:57 -0800

    commit

---
+++ lab6/js/lab6.js
@@ -0,0 +1,19 @@
+/**
+* Author: Jaedon Lee
+* Created: 2.1.2021
+*
+*
+**/
+myTransport = ["walk", "car", "uber"];
+myMainRide = {
+  make: "Toyota",
+  model: "Prius",
+  color: "Black",
+  year: 2005,
+  age: function() {
+    return 2021 - age;
+  }
+}
+document.writeln("How I get around: " + myTransport, "</br");
+document.writeln("myMainRide: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
