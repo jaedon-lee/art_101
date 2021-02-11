fcd86384888ff4a48713dcdf3273d22629594102
jaedon-lee <67574588+jaedon-lee@users.noreply.github.com>
2021-02-10 23:08:52 -0800

    commit

---
+++ lab9/js/lab9.js
@@ -0,0 +1,16 @@
+/**
+* Author: Jaedon Lee
+* Created: 2.10.2021
+*
+*
+**/
+
+var outputEl = document.getElementById("output");
+var nodenew1El = document.createTextNode("random ");
+var nodenew2El = document.createTextNode("text ");
+
+
+
+
+document.getElementById("output").appendChild(nodenew1El);
+document.getElementById("output").appendChild(nodenew2El);
