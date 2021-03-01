ae972f7fa14517eea130ff1f16e5ac09fd0b8ffe
jaedon-lee <67574588+jaedon-lee@users.noreply.github.com>
2021-03-01 15:43:15 -0800

    commit

---
+++ lab13/js/lab13.js
@@ -0,0 +1,30 @@
+/**
+* Author: Jaedon Lee
+* Created: 2.24.2021
+*
+*
+**/
+function fizz(maxNums, factorObj){
+  for (var num = 0; num<maxNums; num++){
+    var outputStr = "";
+    for (var factor in factorObj) {
+      if (num % factor == 0){
+        outputStr += factorObj(factor);
+      }
+    }
+    if (outputStr) {
+      outputStr = " - " + outputStr + "!";
+    }
+    outputToPage(num.toString() + outputStr)
+  }
+}
+function reportError(str){
+  outputEl.innerHTML = "<div class = 'error'>" + str + "</div>";
+}
+document.getElementById("submit").addEventListener("click", function() {
+  var max = document.getElementById("max").value;
+  console.log("max: ", max)
+  if(!max) {
+    reportError("You must provide a maximum")
+  }
+})
